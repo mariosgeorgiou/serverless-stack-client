8356ea311d316599be57e6888d24e7e732aefbc7
marios <mgeorgiou.ece@gmail.com>
2020-04-07 00:53:31 -0400

    Set up Reach app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "marios-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://7t7bg3reue.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_kNpufr5e4",
+    APP_CLIENT_ID: "7l49c0ervu6ffmlme8p8dcg2fv",
+    IDENTITY_POOL_ID: "us-east-1:ff114808-5e37-4292-9f6c-c3035aaab74d"
+  }
+};
